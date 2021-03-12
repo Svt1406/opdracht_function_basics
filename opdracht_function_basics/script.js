8f0fece90b734f15ea530179acd7f1d135ae748f
Svt1406 <79196867+Svt1406@users.noreply.github.com>
2021-03-13 00:48:17 +0100

    Add files via upload

---
+++ opdracht_function_basics/script.js
@@ -0,0 +1,23 @@
+
+const makePizza = function (){
+    console.log('step 1: make the dough');
+    console.log('step 2: add tomatosaus');
+    console.log('step 3: add cheese and whatever toppings you want');
+    console.log('step 4: put the pizza in the oven')
+};
+
+const makeSushi = function (){
+    console.log('step 1: prep the rice & nori')
+    console.log('step 2: spread the rice on the nori')
+    console.log('step 3: add fillings to the bottom quarter of the nori')
+    console.log('step 4:roll the sushi')
+    console.log('step 5:cut the sushi')
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
